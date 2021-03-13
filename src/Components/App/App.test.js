edcc0da651a368f5c9c9a8ad4ef1cdbacb0ae341
Ryan Karumanchery <ryankarumanchery@gmail.com>
2021-03-13 16:44:20 -0500

    Added navbar and product page

--- src/Components/App/App.test.js
+++ src/Components/App/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test link functionalty example", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getByText(/login/i); //TODO: GetById() -> Need to create id/dev-test ids
+  expect(linkElement).toBeInTheDocument(); //TODO: Change to .toBeTruthy()
 });
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,24 +6,55 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import InputBase from "@material-ui/core/InputBase";
+
+const StyledButton = styled(Button)`
+  margin: 0 5px 0 0;
+  color: white;
+`;
+
+const StyledToolbar = styled(Toolbar)`
+  background-color: #403f4c;
+`;
+
+const SearchBar = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0 60px 0;
+`;
+
+const CountrySelection = styled.div`
+  border-style: solid;
+  border-color: grey;
+  background-color: #fdfffc;
+  padding: 4px;
+  margin: 0 20px 0 auto;
+  color: black;
+`;
+
+const StyledInputBase = styled(InputBase)`
+  color: white;
+  padding: 5px;
+`;
 
 function Navbar() {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          className={classes.menuButton}
-          color="inherit"
-          aria-label="menu"
-        >
+      <StyledToolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
           <MenuIcon />
         </IconButton>
-        <Typography variant="h6" className={classes.title}>
-          News
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
+        <Typography variant="h6">Webstore</Typography>
+        <SearchBar>
+          <SearchIcon />
+          <StyledInputBase placeholder="Search..." />
+        </SearchBar>
+        <CountrySelection>Ca</CountrySelection>
+        <StyledButton>Cart</StyledButton>
+        <StyledButton>Login</StyledButton>
+      </StyledToolbar>
     </AppBar>
   );
 }
