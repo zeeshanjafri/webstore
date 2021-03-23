cf1ca3dbb275b4283f89bbbc77437e57067a8578
Ryan Karumanchery <ryankarumanchery@gmail.com>
2021-03-22 22:05:37 -0400

    Added cart and product pages

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,1 +1,20 @@
 // https://wireframe.cc/tpHbo3
+
+import React from "react";
+
+function productPage() {
+  return (
+    <div>
+      <div id="product-img"></div>
+      <div id="product-info">
+        <h2>Product Name</h2>
+        <div>Price($)</div>
+        <div>Specifications</div>
+        <button>Add to cart</button>
+      </div>
+      <div id="related-products">{/* Implement carousel here */}</div>
+    </div>
+  );
+}
+
+export default productPage;
