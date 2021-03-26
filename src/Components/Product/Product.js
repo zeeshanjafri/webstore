6823faa3078c0553f2005ba8d14cbf080e65b3f6
Ryan Karumanchery <ryankarumanchery@gmail.com>
2021-03-25 22:38:33 -0400

    Updated product page

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,19 +1,37 @@
 // https://wireframe.cc/tpHbo3
 
 import React from "react";
+import styled from "styled-components";
+import productImage from "../../Images/watch_test.jpeg";
 
-function productPage() {
+const ProductPage = styled.div`
+  margin: 50px;
+  display: flex;
+  flex-direction: row;
+`;
+
+const ImgContainer = styled.div`
+  img {
+    max-height: 300px;
+    max-width: 200px;
+    margin: 40px;
+  }
+`;
+
+function productPage(props) {
   return (
-    <div>
-      <div id="product-img"></div>
+    <ProductPage>
+      <ImgContainer>
+        <img src={productImage} />
+      </ImgContainer>
       <div id="product-info">
-        <h2>Product Name</h2>
-        <div>Price($)</div>
-        <div>Specifications</div>
+        <h2>Product name - {props.name}</h2>
+        <div>Price($) - {props.price} </div>
+        <div>Specifications - {props.specification} </div>
         <button>Add to cart</button>
       </div>
       <div id="related-products">{/* Implement carousel here */}</div>
-    </div>
+    </ProductPage>
   );
 }
 
