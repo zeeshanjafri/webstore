5925a6dbe2636bf31e8319522a4d9cf63930794e
Ryan Karumanchery <ryankarumanchery@gmail.com>
2021-03-27 23:25:07 -0400

    Styled Cart page

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,16 +1,64 @@
 import React from "react";
+import styled from "styled-components";
+import img1 from "../../Images/watch_test.jpeg";
+
+const CartPage = styled.div`
+  margin-top: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledPaper = styled.div`
+  background-color: #fdfffc;
+  color: black;
+  margin: 10px;
+  width: 8em;
+  height: 10em;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  img {
+    max-height: 100px;
+    width: auto;
+  }
+
+  h3 {
+    text-align: center;
+    margin: 2px 0 30px 0;
+  }
+
+  button {
+    margin: 25px;
+  }
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 function Cart() {
   return (
-    <div>
-      <h2>Shopping cart</h2>
-      <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-        <li>Item 4</li>
-      </ul>
-    </div>
+    <CartPage>
+      <h1>Shopping cart</h1>
+      <StyledDiv>
+        <StyledPaper>
+          <h3>Product 1</h3>
+          <img src={img1} max-width="10em" max-height="10em" />
+        </StyledPaper>
+        <StyledPaper>
+          <h3>Product 1</h3>
+          <img src={img1} max-width="10em" max-height="10em" />
+        </StyledPaper>
+        <StyledPaper>
+          <h3>Product 1</h3>
+          <img src={img1} max-width="10em" max-height="10em" />
+        </StyledPaper>
+      </StyledDiv>
+    </CartPage>
   );
 }
 
